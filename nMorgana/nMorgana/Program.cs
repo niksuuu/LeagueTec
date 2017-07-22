0ef4118ab6e6497d00caf96be4e05523b5f1f08e
Neafle <emredisci2001of@gmail.com>
2017-07-22 13:18:02 +0300

    Initial commit

---
+++ nMorgana/nMorgana/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aimtec;
+using Aimtec.SDK;
+using Aimtec.SDK.Events;
+
+namespace nMorgana
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GameEvents.GameStart += GameEvents_Start;
+		}
+
+		private static void GameEvents_Start()
+		{
+			if (ObjectManager.GetLocalPlayer().ChampionName != "Morgana")
+				return;
+
+			var Morgana = new Morgana();
+
+		}
+	}
+}
